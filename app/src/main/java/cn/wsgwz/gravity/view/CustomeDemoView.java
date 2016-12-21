8813c4b139710d12c57fd12c1a373456815a2ab8
wsgwz <1432818783@qq.com>
2016-12-21 15:47:18 +0800

    1,最新控制

--- app/src/main/java/cn/wsgwz/gravity/view/CustomeDemoView.java
+++ app/src/main/java/cn/wsgwz/gravity/view/CustomeDemoView.java
@@ -1,8 +1,28 @@
 package cn.wsgwz.gravity.view;
-
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.view.*;
 /**
  * Created by Jeremy Wang on 2016/12/21.
  */
 
-public class CustomeDemoView{
+public class CustomeDemoView extends View{
+    public CustomeDemoView(Context context) {
+        super(context);
+    }
+
+    public CustomeDemoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomeDemoView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public CustomeDemoView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
 }
