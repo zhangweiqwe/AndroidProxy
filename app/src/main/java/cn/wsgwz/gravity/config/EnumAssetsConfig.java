5ee3aa599243f23e508b0d4a8742cc1727eacf6e
wsgwz.cn <857899299@qq.com>
2017-01-18 17:43:25 +0800

    1.优化悬浮窗性能，更改ui； 2.更新所有模式，语法[tab]也变成[];

--- app/src/main/java/cn/wsgwz/gravity/config/EnumAssetsConfig.java
+++ app/src/main/java/cn/wsgwz/gravity/config/EnumAssetsConfig.java
@@ -12,6 +12,7 @@ public enum EnumAssetsConfig {
     ChongQing_YiDong_1_S("config/YiDongCaiXingShiPing.xml","移动彩信视频"),
     SiChuan_YiDong_1("config/YiDongShengX.xml","移动圣x"),
     ChongQing_LianTong_1("config/LianTongZhangTing.xml","联通掌厅"),
+    ChongQing_LianTong_10155("config/LianTong10155.xml","联通双host(10155.com)"),
     DianXingTv189_1("config/DianXingTv189_1.xml","电信tv189_1"),
     DianXingTv189_2("config/DianXingTv189_2.xml","电信tv189_2");
 
